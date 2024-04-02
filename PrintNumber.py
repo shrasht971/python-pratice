04c8697c1ab8543ee8484eed1b824e98e39b9e33
shrasht971 <shrashtisahu5@gmail.com>
2024-04-02 22:25:34 +0530

    Add the program display print the number 1 to 10 using while loop

---
+++ PrintNumber.py
@@ -0,0 +1,5 @@
+Write the program display numbers  1 to 10 using  While loop
+i=1
+while i<11:
+    print(i)
+    i=i+1
---
+++ Vowels.py
@@ -0,0 +1,20 @@
+# def vowel(ch):  #user define function
+#     Switcher = {
+#         'a':"Vowel",
+#         'e':"Vowel",
+#         'i':"Vowel",
+#         'o':"Vowel",
+#         'u':"Vowel",
+#         'A':"Vowel",
+#         'E':"Vowel",
+#         'I':"Vowel",
+#         'O':"Vowel",
+#         'U':"Vowel",
+#     }
+#     return Switcher.get(ch,"not vowel")
+
+#     #input from user
+#     ch=input("enter the character:")
+
+#     #calling the function and display the result
+#     print(ch,'is a'+vowel(ch))
