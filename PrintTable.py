b61161bf0f4b8c8546c8adb342e5fb8eeff0c430
shrasht971 <shrashtisahu5@gmail.com>
2024-04-02 20:37:01 +0530

    Add on write the program accept a number and display its table.

---
+++ PrintTable.py
@@ -0,0 +1,6 @@
+# write the program accept a number and display its table.
+# input the user number
+n = int(input("Enter the number:"))
+# use for loop function
+for i in range(1,11):
+    print(n,"*",i,"=",n*i)
